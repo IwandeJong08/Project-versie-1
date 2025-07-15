79ad4693e8b0f2e09e20b083314a91309a201a57
IwandeJong08 <iwandejong2202@gmail.com>
2025-07-15 14:18:34 +0200

    setup boilerplate

---
+++ script.js
@@ -0,0 +1,26 @@
+const CANVAS = document.getElementById('myCanvas');
+const ctx = CANVAS.getContext('2d');
+CANVAS.width = innerWidth;
+CANVAS.height = innerHeight;
+
+
+
+
+
+
+let objecjts = [];
+function init(){
+    
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    // Update and draw objects here
+    objecjts.forEach(object => {
+        object.update();
+    });     
+}
+
+init();
+animate();
